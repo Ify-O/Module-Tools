709ec531772266d30a61946a124a648e1e7c9667
Ifeoma <osegboifeoma3@gmail.com>
2026-03-22 16:37:32 +0100

    Transform input.txt: replace 'i' with 'I' and remove all numbers

--- individual-shell-tools/sed/script-02.sh
+++ individual-shell-tools/sed/script-02.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+sed 's/[0-9]//g' input.txt
+
 # TODO: Write a command to output input.txt with numbers removed.
 # The output should contain 11 lines.
 # Line 6 of the output should be " Alisha".
