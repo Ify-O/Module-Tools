dfa05930f6c97733455668b4ee813ba812144617
Ifeoma <osegboifeoma3@gmail.com>
2026-04-13 13:47:27 +0200

    Implement initial file listing functionality in lsfiles.js

--- implement-shell-tools/ls/sample-files/lsfiles.js
+++ implement-shell-tools/ls/sample-files/lsfiles.js
@@ -1,1 +1,12 @@
 const fs = require("fs");
+
+const dir = process.argv[2] || ".";
+const showAll = process.argv.includes("-a");
+
+let files = fs.readdirSync(dir);
+
+if (!showAll) {
+  files = files.filter((f) => !f.startsWith("."));
+}
+
+console.log(files.join("\n"));
