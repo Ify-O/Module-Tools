85894ffda179cc1150992286a2d4a4ba918bc164
Ifeoma <osegboifeoma3@gmail.com>
2026-04-13 10:45:46 +0200

    Enhance file reading script to output numbered lines from multiple text files

--- implement-shell-tools/cat/sample-files/readfiles.js
+++ implement-shell-tools/cat/sample-files/readfiles.js
@@ -4,5 +4,8 @@ const files = ["1.txt", "2.txt", "3.txt"];
 
 files.forEach((file) => {
   const content = fs.readFileSync(file, "utf-8");
-  console.log(content);
+  const lines = content.split("\n")
+  const numberedLines = lines
+    .map((line, index) => `${index + 1} ${line}`);
+  console.log(numberedLines);
 });
