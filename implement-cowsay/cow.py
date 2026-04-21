bca91a85f9226e0c0b8a481739a5d111119b66e4
Ifeoma <osegboifeoma3@gmail.com>
2026-04-21 07:59:24 +0200

    Add argument parsing for cowsay messages in cow.py

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,3 +1,11 @@
 import cowsay
 import argparse
 
+parser = argparse.ArgumentParser(description="Make animals say things")
+
+parser.add_argument(
+    "message",
+    nargs="+",
+    help="The message to say."
+)
+
