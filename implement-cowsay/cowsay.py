5ad480ec279ca62eda1ded3484a7de398d4b8f7f
Ifeoma <osegboifeoma3@gmail.com>
2026-04-21 08:19:43 +0200

    Add cowsay functionality with argument parsing for messages and animal selection

---
+++ implement-cowsay/cowsay.py
@@ -0,0 +1,23 @@
+import cowsay
+import argparse
+
+parser = argparse.ArgumentParser(description="Make animals say things")
+
+parser.add_argument(
+    "message",
+    nargs="+",
+    help="The message to say."
+)
+
+parser.add_argument(
+    "--animal",
+    choices=cowsay.char_names,  # 🔥 IMPORTANT (dynamic list from library)
+    default="cow",
+    help="The animal to be saying things."
+)
+
+args = parser.parse_args()
+
+text = " ".join(args.message)
+
+getattr(cowsay, args.animal)(text)
